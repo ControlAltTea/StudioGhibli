2a3720289c665fe98e8f423800cf45f913dc0e2b
Tea M <teadevcode@gmail.com>
2022-04-18 19:43:09 -0700

    Scrap scrap scrap
    Can access the films using "includes()" and returns the object containing the fuzzy search of the user's input. Will attempt SudoMateo's drop down pipeline to cycle through pre-determined options instead so that the user does not need to input - simply needs to select from the dropdown menu

--- js/main.js
+++ js/main.js
@@ -1,43 +1,41 @@
 console.log("connected to js")
 
-document.querySelector('button').addEventListener('click',getFetch)
-
+document.querySelector('button').addEventListener('click', getFetch);
 
 
 function getFetch() {
     let userInput = document.querySelector('input').value;
 
-    let movieID = "";
-    
-    const url = `https://ghibliapi.herokuapp.com/films/`;
+
+    const url = new URL("/films","https://ghibliapi.herokuapp.com");
 
     // 2baf70d1-42bb-4437-b551-e5fed5a87abe
+    url.searchParams.set("title", userInput);
 
-    console.log(url);
+    console.log(url.toString());
 
 
     fetch(url)
         .then(res => res.json())
         .then(data => {
+            console.log("DATA is: ", data);
+
+            // Dropdown menu of all the valid movie titles, to pull up their information
+            // go to SudoMateo's 100devs-main, class26, homework, hashicorp-releases
+            // read the js to see the various fetch functions
+            // while loop in fetchVersions deletes the appended childs to refresh the dropdown menus after each new selections
+            // use a forEach to us include and find
+            // fuzzy search, not case sensitive
 
 
-            let movieObj = data;
-
-            console.log(movieObj);
-
-            // hasOwnProperty('propertyName') find object in array
+            // curl -X GET -H "Content-Type: application/json" https://ghibliapi.herokuapp.com/films
 
 
-
-            // if (movieObj.includes() === userInput.value) {
-            //     console.log("Title is found");
-            // }
-
-            document.querySelector('#movieTitle').innerText = movieObj.title;
-            document.querySelector('#releaseDate').innerText = movieObj.release_date;
-            document.querySelector('#directorTitle').innerText = movieObj.director;
-            document.querySelector('img').src = movieObj.movie_banner;
-            document.querySelector('#description').innerText = movieObj.description;
+            document.querySelector('#movieTitle').innerText = data[0].title;
+            document.querySelector('#releaseDate').innerText = data[0].release_date;
+            document.querySelector('#directorTitle').innerText = data[0].director;
+            document.querySelector('img').src = data[0].movie_banner;
+            document.querySelector('#description').innerText = data[0].description;
         })
         .catch(err => {
             console.log(`error ${err}`);
