3cbb77ed80aeda413de919bd83e1bcde864871cb
Tea M <controlalttea@gmail.com>
2022-04-25 14:41:31 -0700

    Was able to request and populate movie information to the DOM. Now working on getting the characters' API request information

--- js/main.js
+++ js/main.js
@@ -3,6 +3,10 @@ console.log("connected to js")
 // OUTER FUNCTION VARIABLES
 const MOVIE_NAME_SELECT = document.querySelector('#movieList');
 const MOVIE_INFO = document.querySelector('#movieInfo')
+const CHARACTER_INFO = document.querySelector('#characterSection')
+
+
+
 // console.log(MOVIE_NAME_SELECT)
 const BASE_URL = new URL("https://ghibliapi.herokuapp.com");
 let url = new URL(`/films`, BASE_URL);
@@ -10,6 +14,7 @@ let url = new URL(`/films`, BASE_URL);
 fetchMovies();
 
 MOVIE_NAME_SELECT.addEventListener("change", fetchMovieInfo);
+MOVIE_NAME_SELECT.addEventListener("change", fetchCharactersInfo);
 
 // Creates a dropdown menu of listed movies in the Studio Ghibli API
 function fetchMovies() {
@@ -23,7 +28,7 @@ function fetchMovies() {
                 let option = document.createElement("option");
                 // option will retain the movie ID value
                 // as well as the text inside
-                option.value = data[movieObj].title;
+                option.value = movieObj;
                 // this will directly add text to the list as it builds
                 option.text = data[movieObj].title;
                 MOVIE_NAME_SELECT.appendChild(option);
@@ -35,9 +40,10 @@ function fetchMovies() {
 // each time a new option from the select menu is chosen, a "change"
 // will be registered and the fetchMovieInfo will run
 function fetchMovieInfo(e){
-    
+    // return's the target's value
+    // not sure how it's only pulling the title...
     const movie = e.target.value;
-    console.log(e.target);
+    console.log(`e is`, e);
     
     // const url = new URL(`${}`)
     console.log(movie);
@@ -46,53 +52,32 @@ function fetchMovieInfo(e){
         .then(resp => resp.json())
         .then(data => {
             MOVIE_INFO.removeAttribute("hidden");
-            console.log(MOVIE_INFO);
+            document.querySelector('#movieTitle').innerText = data[movie].title;
+            document.querySelector('#releaseDate').innerText = data[movie].release_date;
+            document.querySelector('#directorTitle').innerText = data[movie].director;
+            document.querySelector('#movieBanner').src = data[movie].movie_banner;
+            document.querySelector('#description').innerText = data[movie].description;
         })
     
-    document.querySelector('#movieTitle').innerText = movie;
+    fetch(url)
+        .then(resp => resp.json())
+        .then(data => {
+            CHARACTER_INFO.removeAttribute("hidden");
+            //  Object.keys(data).forEach(movieObj => {
+            //     // console.log(MOVIE, data[movieObj]);
+            //     let option = document.createElement("option");
+            //     // option will retain the movie ID value
+            //     // as well as the text inside
+            //     option.value = movieObj;
+            //     // this will directly add text to the list as it builds
+            //     option.text = data[movieObj].title;
+            //     MOVIE_NAME_SELECT.appendChild(option);
+            // })
 
+            Object.keys(data).forEach(movieObj => {
+                let item = document.createElement("li");
+                item.value = movieObj;
+                item.text = data[movieObj].people
+            })
+        })
 }
-
-
-// document.querySelector('button').addEventListener('click', getFetch);
-
-
-// function getFetch() {
-//     let userInput = document.querySelector('input').value;
-
-
-//     const url = new URL("/films","https://ghibliapi.herokuapp.com");
-
-//     // 2baf70d1-42bb-4437-b551-e5fed5a87abe
-//     url.searchParams.set("title", userInput);
-
-//     console.log(url.toString());
-
-
-//     fetch(url)
-//         .then(res => res.json())
-//         .then(data => {
-//             console.log("DATA is: ", data);
-
-//             // Dropdown menu of all the valid movie titles, to pull up their information
-//             // go to SudoMateo's 100devs-main, class26, homework, hashicorp-releases
-//             // read the js to see the various fetch functions
-//             // while loop in fetchVersions deletes the appended childs to refresh the dropdown menus after each new selections
-//             // use a forEach to us include and find
-//             // fuzzy search, not case sensitive
-
-
-//             // curl -X GET -H "Content-Type: application/json" https://ghibliapi.herokuapp.com/films
-
-
-//             document.querySelector('#movieTitle').innerText = data[0].title;
-//             document.querySelector('#releaseDate').innerText = data[0].release_date;
-//             document.querySelector('#directorTitle').innerText = data[0].director;
-//             document.querySelector('img').src = data[0].movie_banner;
-//             document.querySelector('#description').innerText = data[0].description;
-//         })
-//         .catch(err => {
-//             console.log(`error ${err}`);
-//         });
-    
-// }
